051555de04e795ee3a5e4e991a744804f620036c
hendisantika <hendisantika@yahoo.co.id>
2019-06-09 07:58:42 +0700

    #3 - Add UserTO

---
+++ src/main/java/com/hendisantika/springbootnativequerydemo/UserTO.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootnativequerydemo;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-native-query-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-09
+ * Time: 07:58
+ */
+@Data
+public class UserTO {
+
+    private Number id;
+    private String name;
+
+}
